c4f3fbeb2a606c76eb83bd950274e448683b065e
Yidan Wang <yidan.wang@sunnova.com>
2021-08-12 03:54:31 +0000

    second commit

---
+++ models/staging/jaffle_shop/stg_orders.sql
@@ -0,0 +1,14 @@
+WITH
+orders as (
+
+    select
+        id as order_id,
+        user_id as customer_id,
+        order_date,
+        status
+
+    from dbt-tutorial.jaffle_shop.orders
+
+)
+
+SELECT * FROM orders
